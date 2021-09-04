b110458a2ee90c4ba60d01236c5f4921e067d675
Muhammad Mahmudun Nabi <mahmud.nabi@outlook.com>
2021-09-04 17:08:44 +0600

    Initial commit selenium junit

---
+++ src/test/java/TestSeleniumJUnit.java
@@ -0,0 +1,45 @@
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestSeleniumJUnit {
+    WebDriver driver;
+    WebDriverWait wait;
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+        FirefoxOptions ops=new FirefoxOptions();
+        ops.addArguments("--headed");
+        driver=new FirefoxDriver(ops);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+
+@Test
+    public void writeOnTextBox(){
+        driver.get("https://demoqa.com/elements");
+        driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
+        wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Nira");
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+        String text= driver.findElement(By.cssSelector("[id=name]")).getText();
+        Assert.assertTrue(text.contains("Nira"));
+}
+
+    @After
+    public void finishTest(){
+//        driver.close();
+    }
+}
