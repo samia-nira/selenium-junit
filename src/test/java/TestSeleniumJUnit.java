f97d1527073ee70706f0516e15eb5419e62216d4
samia-nira <samiaakternira@gmail.com>
2021-09-05 23:11:45 +0600

    Demoqa commit

--- src/test/java/TestSeleniumJUnit.java
+++ src/test/java/TestSeleniumJUnit.java
@@ -1,16 +1,19 @@
 
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestSeleniumJUnit {
@@ -18,7 +21,7 @@ public class TestSeleniumJUnit {
     WebDriverWait wait;
     @Before
     public void setup(){
-        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
         FirefoxOptions ops=new FirefoxOptions();
         ops.addArguments("--headed");
         driver=new FirefoxDriver(ops);
@@ -27,16 +30,58 @@ public class TestSeleniumJUnit {
     }
 
 
-@Test
+//@Test
+//    public void writeOnTextBox(){
+//        driver.get("https://demoqa.com/elements");
+//        driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
+//        wait = new WebDriverWait(driver, 20);
+//        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Nira");
+//        driver.findElement(By.xpath("//button[@id='submit']")).click();
+//        String text= driver.findElement(By.cssSelector("[id=name]")).getText();
+//        Assert.assertTrue(text.contains("Nira"));
+//}
+
     public void writeOnTextBox(){
         driver.get("https://demoqa.com/elements");
         driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
         wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Nira");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Rahim");
+        //driver.findElement(By.cssSelector("[id=userName]")).sendKeys("Rahim");
         driver.findElement(By.xpath("//button[@id='submit']")).click();
         String text= driver.findElement(By.cssSelector("[id=name]")).getText();
-        Assert.assertTrue(text.contains("Nira"));
-}
+        Assert.assertTrue(text.contains("Rahim"));
+    }
+
+    public void clickOnMultipleButon(){
+        driver.get("https://demoqa.com/buttons");
+        Actions action = new Actions(driver);
+        List<WebElement> list= driver.findElements(By.cssSelector("button"));
+        action.doubleClick(list.get(1)).perform();
+        String text= driver.findElement(By.id("doubleClickMessage")).getText();
+        Assert.assertTrue(text.contains("You have done a double click"));
+
+        action.contextClick(list.get(2)).perform();
+        String text2= driver.findElement(By.id("rightClickMessage")).getText();
+        Assert.assertTrue(text2.contains("You have done a right click"));
+
+        list.get(3).click();
+        String text3= driver.findElement(By.id("dynamicClickMessage")).getText();
+        Assert.assertTrue(text3.contains("You have done a dynamic click"));
+
+    }
+
+
+    @Test
+    public void selectDropdown(){
+        driver.get("https://demoqa.com/select-menu");
+        Select color=new Select(driver.findElement(By.id("oldSelectMenu")));
+        color.selectByValue("7");
+        Select cars=new Select(driver.findElement(By.id("cars")));
+        if (cars.isMultiple()) {
+            cars.selectByValue("volvo");
+            cars.selectByValue("audi");
+        }
+    }
 
     @After
     public void finishTest(){
