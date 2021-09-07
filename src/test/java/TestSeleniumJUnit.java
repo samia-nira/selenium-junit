1dade55fcac02fedca6139aab614140b824b44ad
samia-nira <samiaakternira@gmail.com>
2021-09-07 18:12:28 +0600

    update Demoqa with assignment

--- src/test/java/TestSeleniumJUnit.java
+++ src/test/java/TestSeleniumJUnit.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,15 +14,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 public class TestSeleniumJUnit {
     WebDriver driver;
     WebDriverWait wait;
     @Before
     public void setup(){
-        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "E:\\Practice\\Selenium\\TestSeleniumJunit\\src\\test\\resources/geckodriver.exe");
         FirefoxOptions ops=new FirefoxOptions();
         ops.addArguments("--headed");
         driver=new FirefoxDriver(ops);
@@ -29,29 +33,19 @@ public class TestSeleniumJUnit {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-
 //@Test
-//    public void writeOnTextBox(){
-//        driver.get("https://demoqa.com/elements");
-//        driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
-//        wait = new WebDriverWait(driver, 20);
-//        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Nira");
-//        driver.findElement(By.xpath("//button[@id='submit']")).click();
-//        String text= driver.findElement(By.cssSelector("[id=name]")).getText();
-//        Assert.assertTrue(text.contains("Nira"));
-//}
-
     public void writeOnTextBox(){
         driver.get("https://demoqa.com/elements");
         driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
         wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Rahim");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id=userName]"))).sendKeys("Nira");
         //driver.findElement(By.cssSelector("[id=userName]")).sendKeys("Rahim");
-        driver.findElement(By.xpath("//button[@id='submit']")).click();
+        driver.findElement(By.xpath("//button[@id='submit']")).sendKeys(Keys.ENTER);
         String text= driver.findElement(By.cssSelector("[id=name]")).getText();
-        Assert.assertTrue(text.contains("Rahim"));
+        Assert.assertTrue(text.contains("Nira"));
     }
 
+//    @Test
     public void clickOnMultipleButon(){
         driver.get("https://demoqa.com/buttons");
         Actions action = new Actions(driver);
@@ -70,21 +64,56 @@ public class TestSeleniumJUnit {
 
     }
 
-
-    @Test
+//    @Test
     public void selectDropdown(){
         driver.get("https://demoqa.com/select-menu");
         Select color=new Select(driver.findElement(By.id("oldSelectMenu")));
-        color.selectByValue("7");
+        color.selectByValue("5");
+
         Select cars=new Select(driver.findElement(By.id("cars")));
         if (cars.isMultiple()) {
             cars.selectByValue("volvo");
             cars.selectByValue("audi");
         }
     }
+//@Test
+    public void selectDate(){
+        driver.get("https://demoqa.com/date-picker");
+        driver.findElement(By.id("datePickerMonthYearInput")).clear();
+        driver.findElement(By.id("datePickerMonthYearInput")).sendKeys("04/11/1992");
+        driver.findElement(By.id("datePickerMonthYearInput")).sendKeys(Keys.ENTER);
+    }
+//@Test
+    public void handleAlerts() throws InterruptedException {
+        driver.get("https://demoqa.com/alerts");
+        driver.findElement(By.id(("alertButton"))).click();
+        driver.switchTo().alert().accept();
+        //driver.switchTo().alert().dismiss();
+        driver.findElement(By.id(("promtButton"))).click();
+        driver.switchTo().alert().sendKeys("Nira");
+        sleep(2000);
+        driver.switchTo().alert().accept();
+        String text= driver.findElement(By.id("promptResult")).getText();
+        Assert.assertTrue(text.contains("Nira"));
+    }
+
+  @Test
+  public void handleMultipleTab() throws InterruptedException{
+      driver.get("https://demoqa.com/links");
+      driver.findElement(By.id("simpleLink")).click();
+      sleep(5000);
+      ArrayList<String> w = new ArrayList<String>(driver.getWindowHandles());
+      //switch to open tab
+      driver.switchTo().window(w.get(1));
+      System.out.println("New tab title: " + driver.getTitle());
+      Boolean status = driver.findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).isDisplayed();
+      Assert.assertEquals(true,status);
+      driver.close();
+      driver.switchTo().window(w.get(0));
+  }
 
     @After
     public void finishTest(){
-//        driver.close();
+        driver.close();
     }
 }
